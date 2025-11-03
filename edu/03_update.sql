f10978544f6f60df34d0e7b40813397917a5733f
ByungjooPark <milkpbj23@gmail.com>
2025-11-03 17:33:58 +0900

    251103 update ~ subquery

---
+++ edu/03_update.sql
@@ -0,0 +1,24 @@
+-- UPDATE 문
+UPDATE employees
+SET
+	fire_at = NOW()
+	,deleted_at = NOW()
+WHERE
+	emp_id = 100005
+;
+
+UPDATE salaries
+SET
+	salary = 30000000
+WHERE
+	sal_id = 1022176
+;
+
+
+
+
+
+
+
+
+
